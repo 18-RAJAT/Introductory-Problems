ec5173a845a04b0b49d7fe676bedbab01f789617
Rajat Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-07-09 02:26:55 +0530

    Add files via upload

---
+++ Missing Number/Missing Number.cpp
@@ -0,0 +1,325 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 c++Template.cpp
+*
+* Author : Rajat Joshi
+* Handle: 18-RAJAT
+*
+*/
+
+// Those who cannot remember the past are condemned to repeat it
+
+ 
+/********   All Required Header Files ********/
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define ff(a,b) for(int i=a;i<b;i++)
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define mii map<int,int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// ----------------------<MATH>---------------------------//
+ 
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+int add(int a, int b, int c = MOD){int res=a+b;
+                         return(res>=c?res-c:res);} 
+int mod_neg(int a, int b, int c = MOD){int res;
+                         if(abs(a-b)<c)res=a-b;else res=(a-b)%c;
+                         return(res<0?res+c:res);} 
+int mul(int a, int b, int c = MOD){ll res=(ll)a*b;
+                         return(res>=c?res%c:res);} 
+int muln(int a, int b, int c = MOD){ll res=(ll)a*b;}
+  
+// // Permutation and Combination
+// int ncr(int n,int r,int c = MOD){ 
+//     return mul(mul(ifact[r],ifact[n-r],c),fact[n],c); 
+// }  
+
+// ----------------------</MATH>-------------------------- 
+
+
+
+
+
+// ----------------------</BITWISE>-------------------------- 
+/* a=target variable, b=bit number to act upon 0-n */
+#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+
+// '!!' to make sure this returns 0 or 1
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
+
+#define BITMASK_SET(x, mask) ((x) |= (mask))
+#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
+#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
+#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
+#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
+// ----------------------</BITWISE END>--------------------------//
+
+
+
+
+
+
+/****************** Prime Generator **********************/ 
+const int N=1e7+10; int prime[20000010]; 
+bool isprime[N]; int nprime; 
+template <typename T> void Sieve(T a) 
+{nprime = 0;memset(isprime,true,sizeof(isprime));
+isprime[1]=false;for(int i=2;i<N;i++){
+if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
+isprime[i*j]=false;}}}
+template <typename T> inline T PrimeFactors(T n)
+{ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
+and i<nprime;i++){cnt=0;while(n%prime[i]==0)
+{cnt++;n/=prime[i];}sum*=(cnt+1);}
+if(n>1)sum*=2;return sum;} 
+/**************** Prime Generator End ********************/
+
+
+
+
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+/**********************************************/
+
+
+
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+    int i = 20;
+    char buf[21];
+    // buf[10] = 0;
+    buf[20] = '\n';
+
+    do
+    {
+        buf[--i] = x % 10 + '0';
+        x/= 10;
+    }while(x);
+    do
+    {
+        putchar(buf[i]);
+    } while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+    T n=0,s=1;
+    char p=getchar();
+    if(p=='-')
+        s=-1;
+    while((p<'0'||p>'9')&&p!=EOF&&p!='-')
+        p=getchar();
+    if(p=='-')
+        s=-1,p=getchar();
+    while(p>='0'&&p<='9') {
+        n = (n<< 3) + (n<< 1) + (p - '0');
+        p=getchar();
+    }
+
+    return n*s;
+}
+/************************************/
+
+
+/******* Debugging Class Template *******/
+#define DEBUG
+
+#ifdef DEBUG
+
+    #define debug(args...)     (Debugger()) , args
+
+    class Debugger
+    {
+        public:
+        Debugger(const std::string& _separator = " - ") :
+        first(true), separator(_separator){}
+
+        template<typename ObjectType> Debugger& operator , (const ObjectType& v)
+        {
+            if(!first)
+                std:cerr << separator;
+            std::cerr << v;
+            first = false;
+            return *this;
+        }
+        ~Debugger() {  std:cerr << endl;}
+
+        private:
+        bool first;
+        std::string separator;
+    };
+
+#else
+    #define debug(args...)                  // Just strip off all debug tokens
+#endif
+
+/**************************************/
+
+/******** User-defined Function *******/
+
+
+/**************************************/
+
+
+// /********** Main()  function **********/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
+        
+    long long n,s=0,e;cin >> n;
+    for (int i=0;i<n-1;++i)
+    {cin>>e;s=s+e;}
+    cout<<(n*(n+1))/2-s;
+}
